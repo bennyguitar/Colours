c4b79f4227c2936a2fd21d3d970304cb83868b6f
BenGordon <brgordon@ua.edu>
2013-03-18 12:28:17 -0500

    Added a bunch of new, predefined colors

--- Colours.h
+++ Colours.h
@@ -26,6 +26,12 @@
 #define ColorSteelBlue [UIColor colorWithRed:103/255.0f green:153/255.0f blue:170/255.0f alpha:1.0]
 #define ColorRobinEgg [UIColor colorWithRed:141/255.0f green:218/255.0f blue:247/255.0f alpha:1.0]
 #define ColorPastelBlue [UIColor colorWithRed:99/255.0f green:161/255.0f blue:247/255.0f alpha:1.0]
+#define ColorTurquoise [UIColor colorWithRed:112/255.0f green:219/255.0f blue:219/255.0f alpha:1.0]
+#define ColorSkyBlue [UIColor colorWithRed:0/255.0f green:178/255.0f blue:238/255.0f alpha:1.0]
+#define ColorIndigo [UIColor colorWithRed:13/255.0f green:79/255.0f blue:139/255.0f alpha:1.0]
+#define ColorDenim [UIColor colorWithRed:67/255.0f green:114/255.0f blue:170/255.0f alpha:1.0]
+#define ColorBlueberry [UIColor colorWithRed:89/255.0f green:113/255.0f blue:173/255.0f alpha:1.0]
+#define ColorCornflower [UIColor colorWithRed:100/255.0f green:149/255.0f blue:237/255.0f alpha:1.0]
 
 // Greens
 #define ColorEmerald [UIColor colorWithRed:1/255.0f green:152/255.0f blue:117/255.0f alpha:1.0]
@@ -33,6 +39,11 @@
 #define ColorPastelGreen [UIColor colorWithRed:126/255.0f green:242/255.0f blue:124/255.0f alpha:1.0]
 #define ColorSeafoam [UIColor colorWithRed:77/255.0f green:226/255.0f blue:140/255.0f alpha:1.0]
 #define ColorPaleGreen [UIColor colorWithRed:176/255.0f green:226/255.0f blue:172/255.0f alpha:1.0]
+#define ColorCactusGreen [UIColor colorWithRed:99/255.0f green:111/255.0f blue:87/255.0f alpha:1.0]
+#define ColorChartreuse [UIColor colorWithRed:69/255.0f green:139/255.0f blue:0/255.0f alpha:1.0]
+#define ColorHollyGreen [UIColor colorWithRed:32/255.0f green:87/255.0f blue:14/255.0f alpha:1.0]
+#define ColorOlive [UIColor colorWithRed:91/255.0f green:114/255.0f blue:34/255.0f alpha:1.0]
+#define ColorMoneyGreen [UIColor colorWithRed:134/255.0f green:198/255.0f blue:124/255.0f alpha:1.0]
 
 // Reds
 #define ColorSalmon [UIColor colorWithRed:233/255.0f green:87/255.0f blue:95/255.0f alpha:1.0]
@@ -40,12 +51,23 @@
 #define ColorEasterPink [UIColor colorWithRed:241/255.0f green:167/255.0f blue:162/255.0f alpha:1.0]
 #define ColorGrapefruit [UIColor colorWithRed:228/255.0f green:31/255.0f blue:54/255.0f alpha:1.0]
 #define ColorPink [UIColor colorWithRed:255/255.0f green:95/255.0f blue:154/255.0f alpha:1.0]
+#define ColorIndianRed [UIColor colorWithRed:205/255.0f green:92/255.0f blue:92/255.0f alpha:1.0]
+#define ColorStrawberry [UIColor colorWithRed:190/255.0f green:38/255.0f blue:37/255.0f alpha:1.0]
+#define ColorCoral [UIColor colorWithRed:240/255.0f green:128/255.0f blue:128/255.0f alpha:1.0]
+#define ColorMaroon [UIColor colorWithRed:80/255.0f green:4/255.0f blue:28/255.0f alpha:1.0]
+#define ColorWatermelon [UIColor colorWithRed:242/255.0f green:71/255.0f blue:63/255.0f alpha:1.0]
 
 // Purples
 #define ColorEggplant [UIColor colorWithRed:105/255.0f green:5/255.0f blue:98/255.0f alpha:1.0]
 #define ColorPastelPurple [UIColor colorWithRed:207/255.0f green:100/255.0f blue:235/255.0f alpha:1.0]
 #define ColorPalePurple [UIColor colorWithRed:229/255.0f green:180/255.0f blue:235/255.0f alpha:1.0]
 #define ColorCoolPurple [UIColor colorWithRed:140/255.0f green:93/255.0f blue:228/255.0f alpha:1.0]
+#define ColorViolet [UIColor colorWithRed:191/255.0f green:95/255.0f blue:255/255.0f alpha:1.0]
+#define ColorPlum [UIColor colorWithRed:139/255.0f green:102/255.0f blue:139/255.0f alpha:1.0]
+#define ColorLavender [UIColor colorWithRed:204/255.0f green:153/255.0f blue:204/255.0f alpha:1.0]
+#define ColorRaspberry [UIColor colorWithRed:135/255.0f green:38/255.0f blue:87/255.0f alpha:1.0]
+#define ColorFuschia [UIColor colorWithRed:255/255.0f green:20/255.0f blue:147/255.0f alpha:1.0]
+#define ColorGrape [UIColor colorWithRed:54/255.0f green:11/255.0f blue:88/255.0f alpha:1.0]
 
 // Yellows
 #define ColorGoldenrod [UIColor colorWithRed:215/255.0f green:170/255.0f blue:51/255.0f alpha:1.0]
