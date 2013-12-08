117c4c0a977313e2521cc20b06a3ff5355a0e5ad
brgordon <brgordon@ua.edu>
2013-12-08 15:50:12 -0600

    Added self-documentation to the .h file

--- UIColor+Colours.h
+++ UIColor+Colours.h
@@ -20,24 +20,95 @@ typedef enum
 
 @interface UIColor (Colours)
 
-// Color Methods
+#pragma mark - Color from Hex/RGBA/HSBA
+/**
+ Creates a UIColor from a Hex representation string
+ @param hexString   Hex string that looks like @"#FF0000" or @"FF0000"
+ @return    UIColor
+ */
 + (UIColor *)colorFromHexString:(NSString *)hexString;
+
+/**
+ Creates a UIColor from an array of 4 NSNumbers (r,g,b,a)
+ @param rgbaArray   4 NSNumbers for rgba between 0 - 1
+ @return    UIColor
+ */
 + (UIColor *)colorFromRGBAArray:(NSArray *)rgbaArray;
+
+/**
+ Creates a UIColor from a dictionary of 4 NSNumbers
+ Keys: @"r",@"g",@"b",@"a"
+ @param rgbaDictionary   4 NSNumbers for rgba between 0 - 1
+ @return    UIColor
+ */
 + (UIColor *)colorFromRGBADictionary:(NSDictionary *)rgbaDict;
+
+/**
+ Creates a UIColor from an array of 4 NSNumbers (h,s,b,a)
+ @param hsbaArray   4 NSNumbers for rgba between 0 - 1
+ @return    UIColor
+ */
 + (UIColor *)colorFromHSBAArray:(NSArray *)hsbaArray;
+
+/**
+ Creates a UIColor from a dictionary of 4 NSNumbers
+ Keys: @"h",@"s",@"b",@"a"
+ @param hsbaDictionary   4 NSNumbers for rgba between 0 - 1
+ @return    UIColor
+ */
 + (UIColor *)colorFromHSBADictionary:(NSDictionary *)hsbaDict;
+
+
+#pragma mark - Hex/RGBA/HSBA from Color
+/**
+ Creates a Hex representation from a UIColor
+ @return    NSString
+ */
 - (NSString *)hexString;
+
+/**
+ Creates an array of 4 NSNumbers representing the float values of r, g, b, a in that order.
+ @return    NSArray
+ */
 - (NSArray *)rgbaArray;
+
+/**
+ Creates an array of 4 NSNumbers representing the float values of h, s, b, a in that order.
+ @return    NSArray
+ */
 - (NSArray *)hsbaArray;
+
+/**
+ Creates a dictionary of 4 NSNumbers representing float values with keys: "r", "g", "b", "a"
+ @return    NSDictionary
+ */
 - (NSDictionary *)rgbaDictionary;
+
+/**
+ Creates a dictionary of 4 NSNumbers representing float values with keys: "h", "s", "b", "a"
+ @return    NSDictionary
+ */
 - (NSDictionary *)hsbaDictionary;
 
-// Generate Color Scheme
+
+#pragma mark - 4 Color Scheme from Color
+/**
+ Creates an NSArray of 4 UIColors that complement the UIColor.
+ @param type ColorSchemeAnalagous, ColorSchemeMonochromatic, ColorSchemeTriad, ColorSchemeComplementary
+ @return    NSArray
+ */
 - (NSArray *)colorSchemeOfType:(ColorScheme)type;
 
-// Contrasting Color
+
+#pragma mark - Contrasting Color from Color
+/**
+ Creates either [UIColor whiteColor] or [UIColor blackColor] depending on if the color this method is run on is dark or light.
+ @return    UIColor
+ */
 - (UIColor *)blackOrWhiteContrastingColor;
-    
+
+
+#pragma mark - Colors
 // System Colors
 + (UIColor *)infoBlueColor;
 + (UIColor *)successColor;
