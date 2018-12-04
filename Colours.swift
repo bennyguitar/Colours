1db6174ec4a239394333b993a67def5fd83d4042
Yang Yubo <yang@yangyubo.com>
2018-12-04 15:10:34 +0800

    Create Color from hex integer

--- Colours.swift
+++ Colours.swift
@@ -36,16 +36,24 @@ public extension Color {
         case darkness = 0, lightness, desaturated, saturated, red, green, blue
     }
     
-    
     // MARK: - Color from Hex/RGBA/HSBA/CIE_LAB/CMYK
     convenience init(hex: String, alpha: CGFloat = 1.0) {
         var rgbInt: UInt64 = 0
         let newHex = hex.replacingOccurrences(of: "#", with: "")
         let scanner = Scanner(string: newHex)
         scanner.scanHexInt64(&rgbInt)
-        let r: CGFloat = CGFloat((rgbInt & 0xFF0000) >> 16)/255.0
-        let g: CGFloat = CGFloat((rgbInt & 0x00FF00) >> 8)/255.0
-        let b: CGFloat = CGFloat(rgbInt & 0x0000FF)/255.0
+        self.init(hex: rgbInt, alpha: alpha)
+    }
+    
+    /**
+     Creates a color from an hex integer (e.g. 0x3498db).
+     - parameter hex: A hexa-decimal UInt32 that represents a color.
+     - parameter alpha: The alpha channel.
+     */
+    convenience init(hex: UInt64, alpha: CGFloat = 1.0) {
+        let r: CGFloat = CGFloat((hex & 0xFF0000) >> 16)/255.0
+        let g: CGFloat = CGFloat((hex & 0x00FF00) >> 8)/255.0
+        let b: CGFloat = CGFloat(hex & 0x0000FF)/255.0
         self.init(red: r, green: g, blue: b, alpha: alpha)
     }
     
